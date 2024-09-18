68e6e4ca74726d454245a925534bc0a20efd366b
igorlage <igor-rl@hotmail.com>
2024-09-18 07:44:08 -0300

    chore: home page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,14 @@
+function HomePage() {
+	return (
+		<>
+			<h1>Home Page</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+				excepturi quos tempora doloremque vero atque dolorum in rem cumque
+				incidunt, quas velit! Nisi molestiae assumenda suscipit doloribus, rem
+				provident ratione!
+			</p>
+		</>
+	);
+}
+export default HomePage;
